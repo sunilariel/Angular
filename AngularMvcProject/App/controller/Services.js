bd4096220e932da08ec1b2ef072b458003cac6bf
Ariel-39-PC\Ariel-39 <ariel.amitc@gmail.com>
2017-06-26 10:34:36 +0530

    Changes related to Service and Staff Page.

---
+++ AngularMvcProject/App/controller/Services.js
@@ -0,0 +1,15 @@
+﻿app.controller("servicesController", ['$scope', '$http', '$routeParams', '$timeout', '$location', function ($scope, $http, $routeParams, $timeout, $location) {
+
+    $scope.redirecttoCustomer = function () {
+        $location.path("/customer/" + $routeParams.CompanyId);
+    }
+    $scope.RedirecttoStaff = function () {
+        $location.path("/Setting/" + $routeParams.CompanyId);
+    }
+    $scope.redirecttodashboard = function () {        
+        $location.path("/dashboard/" + $routeParams.CompanyId);
+    }
+    $scope.redirecttoServices = function () {
+        $location.path("/Services/" + $routeParams.CompanyId);
+    }
+}]);
---
+++ AngularMvcProject/App/controller/Staff.js
@@ -0,0 +1,23 @@
+﻿app.controller("staffController", ['$scope', '$http', '$routeParams', '$timeout', '$location', function ($scope, $http, $routeParams, $timeout, $location) {
+
+
+    $scope.showstaffpopup = false;
+    //Redirection//
+
+    $scope.redirecttoCustomer = function () {
+        $location.path("/customer/" + $routeParams.CompanyId);
+    }
+    $scope.RedirecttoStaff = function () {
+        $location.path("/Setting/" + $routeParams.CompanyId);
+    }
+    $scope.redirecttodashboard = function () {
+        debugger;
+        $location.path("/dashboard/" + $routeParams.CompanyId);
+    }
+    $scope.redirecttoServices=function()
+    {
+        $location.path("/Services/" + $routeParams.CompanyId);
+    }
+
+
+}]);
